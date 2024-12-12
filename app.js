6bc15c1cd3506576fb71d69e88a9af8c0440db51
Duong Minh Quang <minhquangduong933@gmail.com>
2024-12-12 21:43:52 +0700

    update

---
+++ app.js
@@ -0,0 +1,105 @@
+  const productsitem = [
+    { 
+    id:"1",
+      imgUrl:"https://images.unsplash.com/photo-1548741487-18d363dc4469?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Chocolate tươi 1 hộp 408 gram",
+      price:"376000",
+      des:"Chocolate tươi là sản phẩm hoàn hảo dành cho những ai yêu thích hương vị ngọt ngào, mịn màng và sự kết hợp tuyệt vời của cacao nguyên chất. Được chế biến từ nguồn cacao chọn lọc, chocolate tươi này mang đến cho bạn trải nghiệm thưởng thức đầy đậm đà, béo ngậy, nhưng vẫn thanh thoát và không quá ngọt.Mỗi hộp chocolate tươi chứa các viên chocolate được làm thủ công, đảm bảo chất lượng và độ tươi mới. Sản phẩm này lý tưởng để dùng làm quà tặng, thưởng thức cùng gia đình, bạn bè trong những dịp đặc biệt hoặc đơn giản là để tự thưởng cho bản thân sau một ngày dài.",
+    },{
+    id:"2",
+      imgUrl:"https://images.unsplash.com/photo-1583312228158-4001fca6e316?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Chocolate Belgium",
+      price:"320000",
+      des:"Chocolate Belgium là một món quà tuyệt vời của nghệ thuật làm chocolate, mang đến hương vị đậm đà và tinh tế, được chế biến từ cacao chất lượng cao, theo công thức truyền thống của người Bỉ. Được biết đến với sự tinh xảo và hương vị vượt trội, chocolate Bỉ luôn là sự lựa chọn lý tưởng cho những ai yêu thích sự hoàn hảo trong từng viên chocolate.Sản phẩm Chocolate Belgium được sản xuất từ những hạt cacao nguyên chất, kết hợp với sữa tươi và các thành phần tự nhiên, mang đến hương vị vừa ngọt ngào, vừa đậm đà mà không quá ngấy. Mỗi viên chocolate đều được chế biến một cách tỉ mỉ, tạo ra một trải nghiệm thưởng thức tuyệt vời, mịn màng và tan chảy trong miệng.",
+    },{
+    id:"3",
+      imgUrl:"https://images.unsplash.com/photo-1623000850229-ad83232ea6c7?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Chocolate đen 100%",
+      price:"100000",
+      des:"Chocolate đen 100% là lựa chọn hoàn hảo dành cho những tín đồ yêu thích sự mạnh mẽ và đậm đà của cacao nguyên chất. Được làm từ 100% cacao, sản phẩm này mang lại hương vị đậm, đắng tự nhiên với độ cân bằng hoàn hảo giữa vị cacao nguyên chất và sự tinh tế trong từng miếng chocolate. Không có thêm đường hay bất kỳ chất phụ gia nào, chocolate đen 100% giữ trọn vẹn bản chất của cacao, là lựa chọn lý tưởng cho những ai yêu thích sự thuần khiết và mạnh mẽ trong hương vị. Sản phẩm không chỉ hấp dẫn bởi hương vị đặc biệt mà còn mang lại nhiều lợi ích cho sức khỏe, như cung cấp chất chống oxy hóa, hỗ trợ sức khỏe tim mạch và cải thiện tinh thần. Với thành phần hoàn toàn tự nhiên, chocolate đen 100% còn là một món ăn vặt lý tưởng cho những người theo chế độ ăn kiêng, hoặc những ai cần kiểm soát lượng đường trong khẩu phần ăn.",
+    },{
+    id:"4",
+      imgUrl:"https://images.unsplash.com/photo-1582042837973-a13faf7e0eb4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Chocolate Lindt",
+      price:"200000",
+      des:"Chocolate Lindt là biểu tượng của sự tinh tế và chất lượng cao trong ngành chocolate, nổi tiếng với hương vị đậm đà, mịn màng và kết cấu hoàn hảo. Được sản xuất bởi thương hiệu Lindt - một trong những nhà sản xuất chocolate hàng đầu thế giới từ Thụy Sĩ, sản phẩm này luôn mang lại trải nghiệm tuyệt vời cho những tín đồ yêu thích chocolate cao cấp. Chocolate Lindt được chế biến từ những hạt cacao nguyên chất được chọn lọc kỹ lưỡng, kết hợp với công thức pha trộn độc đáo, mang lại hương vị ngọt ngào nhưng không quá ngấy, với độ mịn màng và tan chảy tuyệt vời trong miệng. Các sản phẩm của Lindt luôn nổi bật nhờ vào chất lượng tuyệt hảo và sự chăm chút trong từng chi tiết, từ việc lựa chọn nguyên liệu đến quy trình sản xuất.",
+    },{
+    id:"5",
+      imgUrl:"https://images.unsplash.com/photo-1674069686288-071c6e4ef838?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Kẹo dẻo cầu vồng",
+      price:"70000",
+      des:"Kẹo dẻo cầu vồng là một món ăn vặt ngọt ngào, đầy màu sắc, mang đến niềm vui cho mọi lứa tuổi. Với sự kết hợp hoàn hảo của các màu sắc rực rỡ và hương vị trái cây tươi mát, mỗi viên kẹo dẻo cầu vồng là một trải nghiệm thú vị, vừa mềm mại vừa sảng khoái, khiến bạn không thể dừng lại khi đã thử. Kẹo dẻo cầu vồng được làm từ những nguyên liệu chất lượng cao, đảm bảo độ dẻo và mềm mịn. Mỗi viên kẹo mang đến một sự kết hợp hương vị trái cây đa dạng, từ dâu, cam, chanh đến táo và nho, giúp kích thích vị giác và làm mới tâm trạng. Với sự pha trộn hoàn hảo giữa vị ngọt và chua nhẹ, kẹo dẻo cầu vồng luôn là sự lựa chọn lý tưởng cho những lúc muốn thưởng thức món ăn vặt vui nhộn, hoặc làm quà tặng đầy bất ngờ.",
+    },{
+    id:"6",
+      imgUrl:"https://images.unsplash.com/photo-1719825440643-5662470e5964?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Kẹo dẻo, bánh khoai môn, bánh quy các loại",
+      price:"150000",
+      des:"Bánh khoai môn là món bánh truyền thống với hương vị thơm ngon, béo ngậy từ khoai môn và các nguyên liệu tự nhiên. Vỏ bánh mềm mịn, kết hợp với nhân khoai môn thơm bùi, tạo nên một món ăn vặt vô cùng hấp dẫn. Với hương vị độc đáo và cách chế biến công phu, bánh khoai môn thích hợp làm món tráng miệng trong các bữa ăn gia đình, hay là món quà biếu ý nghĩa cho bạn bè và người thân.",
+    },{
+    id:"7",
+      imgUrl:"https://images.unsplash.com/photo-1729478673847-1c1da37f86f6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Bánh quy ngọt các loại",
+      price:"120000",
+      des:"Bánh quy ngọt các loại là món ăn vặt thơm ngon, nhẹ nhàng, thích hợp cho mọi lứa tuổi và mọi dịp. Được làm từ những nguyên liệu chất lượng, bánh quy mang đến hương vị ngọt ngào, giòn tan, với nhiều kiểu dáng và hương vị đa dạng, từ bánh quy bơ truyền thống, bánh quy sô cô la, đến các loại bánh quy với hương vị trái cây tươi ngon. Với mỗi miếng bánh, bạn sẽ cảm nhận được sự hòa quyện hoàn hảo giữa vị ngọt nhẹ nhàng và độ giòn rụm, tạo nên một trải nghiệm ẩm thực thú vị. Bánh quy ngọt các loại không chỉ là món ăn vặt yêu thích mà còn là lựa chọn lý tưởng để kết hợp cùng trà, cà phê hay các loại đồ uống khác trong những buổi tiệc, họp mặt gia đình, hay trong các buổi nghỉ ngơi thư giãn.",
+    },{
+    id:"8",
+      imgUrl:"https://plus.unsplash.com/premium_photo-1677607979859-126a98970ad4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Raspberry Lemon Cake",
+      price:"350000",
+      des:"Raspberry Lemon Cake (Bánh Mâm Xôi Chanh) là sự kết hợp hoàn hảo giữa vị ngọt nhẹ nhàng của mâm xôi tươi và vị chua thanh mát của chanh, tạo nên một chiếc bánh mềm mịn, thơm ngon và đầy hấp dẫn. Với lớp bánh xốp, mịn màng, được kết hợp cùng lớp kem chanh tươi mát và mâm xôi chín mọng, mỗi miếng bánh đều mang lại sự tươi mới, dễ chịu và kích thích vị giác. Bánh Raspberry Lemon Cake không chỉ là món tráng miệng thơm ngon mà còn mang đến một trải nghiệm ẩm thực thú vị nhờ vào sự cân bằng hoàn hảo giữa vị ngọt và chua. Mâm xôi tươi làm tăng thêm độ chua nhẹ nhàng, còn chanh lại mang đến sự tươi mát, tạo nên một hương vị rất dễ chịu và dễ ăn.",
+    }
+    
+  ];
+  function renderProductlist() {
+    
+    const productList = document.getElementById("product-list");
+
+    // Clear any previous product list
+    productList.innerHTML = ''; 
+
+    productsitem.forEach((product) => {
+        const divElement = document.createElement("div");
+        divElement.className = "product-item";
+        divElement.innerHTML = `
+            <div class="product-image"><img id="product-${product.id}" src="${product.imgUrl}" alt="${product.name}"></div>
+            <div class="product-info">
+                <h1 class="product-title">${product.name}</h1>
+                <p class="product-price">${product.price} VNĐ</p>
+                <p class="product-description">${product.des}</p>
+            </div>
+        `;
+        productList.appendChild(divElement);
+    });
+  $(document).ready(function(){
+    $('#product-list').slick({
+        slidesToShow: 1,
+        arrows:true,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow '><i class='fa fa-angle-left' aria-hidden='true'></i></button>`,
+      nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>`,
+        dots:true,
+    });
+    var urlParams = new URLSearchParams(window.location.search);
+    var slideIndex = urlParams.get('slide');  // Lấy tham số 'slide' từ URL
+
+    // Nếu tham số 'slide' có, di chuyển carousel đến slide đó
+    if (slideIndex !== null) {
+      $('.image-slider').slick('slickGoTo', parseInt(slideIndex));  // Di chuyển đến slide tương ứng
+    }
+    Slick.prototype.goTo = Slick.prototype.slickGoTo = function(slide, dontAnimate) {
+
+      var _ = this;
+
+      _.changeSlide({
+          data: {
+              message: 'index',
+              index: parseInt(slide)
+          }
+      }, dontAnimate);
+
+  };
+
+  });
+}
+  window.onload = function() {
+    renderProductlist();
+}
--- java/java.js
+++ java/java.js
@@ -26,42 +26,61 @@ function scrollToTop() {
       nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>`,
       dots:true,
     });
+    $('a[data-slide]').click(function(e) {
+      e.preventDefault();  // Ngừng hành động mặc định của liên kết
+
+      var slideIndex = $(this).data('slide');  // Lấy chỉ số slide từ thuộc tính data-slide
+      var targetUrl = "banhkhoaimon.html?slide=" + slideIndex;  // Xây dựng URL với tham số slide
+
+      // Chuyển hướng đến trang mới với tham số slide
+      window.location.href = targetUrl;
+    });
   });
  
  
   const products = [
-    {
+    { 
+      id:"1",
       imgUrl:"https://images.unsplash.com/photo-1548741487-18d363dc4469?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      page:"chocolatebox.html",
       name: "Chocolate tươi 1 hộp 408 gram",
-      price:"476000",
+      price:"376000",
     },{
+      id:"2",
       imgUrl:"https://images.unsplash.com/photo-1583312228158-4001fca6e316?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      page:"belgium.html",
       name: "Chocolate Belgium",
       price:"320000",
     },{
-      imgUrl:"https://images.unsplash.com/photo-1623000850229-ad83232ea6c7?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://images.unsplash.com/photo-1623000850229-ad83232ea6c7?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"3",
+      page:"chocblack.html",
       name: "Chocolate đen 100%",
       price:"100000",
     },{
-      imgUrl:"https://images.unsplash.com/photo-1582042837973-a13faf7e0eb4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://images.unsplash.com/photo-1582042837973-a13faf7e0eb4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"4",
+      page:"lindt.html",
       name: "Chocolate Lindt",
       price:"200000",
     },{
-      imgUrl:"https://images.unsplash.com/photo-1674069686288-071c6e4ef838?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://images.unsplash.com/photo-1674069686288-071c6e4ef838?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"5",
       name: "Kẹo dẻo cầu vồng",
+      page:"keodeo.html",
       price:"70000",
     },{
-      imgUrl:"https://images.unsplash.com/photo-1719825440643-5662470e5964?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://images.unsplash.com/photo-1719825440643-5662470e5964?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"6",
       name: "Kẹo dẻo, bánh khoai môn, bánh quy các loại",
+      page:"banhkhoaimon.html",
       price:"150000",
     },{
-      imgUrl:"https://images.unsplash.com/photo-1729478673847-1c1da37f86f6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://images.unsplash.com/photo-1729478673847-1c1da37f86f6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"7",
       name: "Bánh quy ngọt các loại",
       price:"120000",
+      page:"banhngot.html",
     },{
-      imgUrl:"https://plus.unsplash.com/premium_photo-1677607979859-126a98970ad4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      imgUrl:"https://plus.unsplash.com/premium_photo-1677607979859-126a98970ad4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",id:"8",
       name: "Raspberry Lemon Cake",
       price:"350000",
+      page:"raspberrylemoncake.html",
     }
     
   ];
@@ -73,7 +92,7 @@ function scrollToTop() {
       divElement.className = "image-item";
       divElement.innerHTML = `
               <div class="image-item">
-                <a href="">
+                <a href="banhkhoaimon.html#product-${product.id}" target="_blank">
                 <div class="image"><img src="${product.imgUrl}" alt="">
                 </div></a>
                 <h3 class="image-title">${product.name}</h3>
@@ -148,8 +167,8 @@ function scrollToTop() {
     const cartIcon = document.querySelector("#cart-logo");
 
     // Kiểm tra nếu cartElement và cartIcon tồn tại
-    if (!cartElement.contains(event.target) && !cartIcon.contains(event.target)) {
-    cartElement.style.display = "none";
+    if (cartElement.contains(event.target) && cartIcon.contains(event.target)) {
+    cartElement.style.display = "block";
 
     
   }
